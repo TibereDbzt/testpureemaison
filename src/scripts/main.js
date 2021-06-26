1eaff7cd0d1e9e91d1c4bc707dd1ed48faf0d19c
debizett <tibere.debizet@etu.umontpellier.fr>
2021-06-26 15:23:01 +0200

    hero section integration

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 import '../styles/main.sass';
+import bg from './../assets/images/dark_clouds.jpeg';
+
+const init = () => {
+    document.querySelector('[data-bgImage]').src = bg;
+    const elem = document.querySelector('.target')
+    console.log(elem);
+    console.log(elem.getBoundingClientRect());
+}
+
+document.addEventListener('load', init());
